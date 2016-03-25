e258c6750dd6b8ad2c65e64e51843105a67288e5
Anndo <handcuffanndo@gmail.com>
2016-03-26 03:44:54 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ScoreCalculator
+//
+//  Created by Yu-An Ko on 2016/3/26.
+//  Copyright © 2016年 Yu-An Ko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
