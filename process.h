18d5c71e8fcb85cc0096346bc394e83306dac153
Anndo <handcuffanndo@gmail.com>
2016-03-27 21:02:51 +0800

    Fix: rewrite avg

--- process.h
+++ process.h
@@ -17,45 +17,57 @@
 using namespace std;
 
 void readFile(){
+    
     int i, n;
-    struct student *s;
-    FILE *input;
-    input = fopen ("list2.txt", "r");
     
     cout << "Number of student: "; // prompt
     cin >> n; // n: number of students
     
+    struct student *s;
     s = new struct student[n];
 
 /*
---------------------READ FILE AND AVG.--------------------
+--------------------READ FILE--------------------
 */
 
+    FILE *input;
+    input = fopen ("list2.txt", "r");
+
+    // error message
     if (input == NULL)
     {
         cout << "Failed to open the file.\n";
     }
     
-    // name the columns: #, NAME, AGE, SCORE1, SCORE2, SCORE3, AVG.
-    cout << "#" << setw(12) << "NAME" << setw(8) << "AGE"<< setw(8) << "SCORE1" << setw(8) << "SCORE2" << setw(8) << "SCORE3" << setw(8) << "AVG." << endl;
-    cout << "-----------------------------------------------------" << endl;
-    
+    // input from a txt. file
     for (i = 0; i < n; i++)
     {
-        // input from a txt. file
         fscanf (input, "%79s %d %d %d %d", s[i].name, &s[i].age, &s[i].score1, &s[i].score2, &s[i].score3);
-        
-        // average
-        s[i].avg = (s[i].score1 + s[i].score1 + s[i].score1) / 3;
-
-        // print results
-        cout << i+1 << setw(12) << s[i].name << setw(8) << s[i].age << setw(8) << s[i].score1 << setw(8) << s[i].score2 << setw(8) << s[i].score3 << setw(8) << s[i].avg << endl;
-        cout << "-----------------------------------------------------" << endl;
+       }
+    
+/*
+--------------------AVG.--------------------
+*/
+    for (i = 0; i < n; i++)
+    {
+        double avg = (s[i].score1 + s[i].score2 + s[i].score3) / 3.0;
+        s[i].avg = avg;
+    }
+    
+    // name the columns: #, NAME, AGE, SCORE1, SCORE2, SCORE3, AVG.
+    cout << "\n\n#" << setw(12) << "NAME" << setw(8) << "AGE"<< setw(8) << "SCORE1" << setw(8) << "SCORE2" << setw(8) << "SCORE3" << setw(12) << "AVG." << endl;
+    cout << "----------------------------------------------------------" << endl;
+    
+    // print results
+    for (i = 0; i < n; i++){
+        cout << i+1 << setw(12) << s[i].name << setw(8) << s[i].age << setw(8) << s[i].score1 << setw(8) << s[i].score2 << setw(8) << s[i].score3 << setw(12) << s[i].avg << endl;
+        cout << "----------------------------------------------------------" << endl;
     }
     
 /*
 --------------------find MAX--------------------
 */
+    
     int max = s[0].avg;
     string  max_stu;
     for (i = 0; i < n; i++){
@@ -65,10 +77,11 @@ void readFile(){
         }
     }
     cout << "\nMAX. SCORE: " << max << "\nSTUDENT: " << max_stu << endl;
-
+    
 /*
 --------------------find MIN--------------------
 */
+    
     int min = s[0].avg;
     string  min_stu;
     for (i = 0; i < n; i++){
@@ -79,30 +92,12 @@ void readFile(){
     }
     cout << "\nMIN. SCORE: " << min << "\nSTUDENT: " << min_stu << endl;
     
-
+/*
+--------------------Bubble Sort--------------------
+*/
     cout << "\n\n";
     fclose(input);
 }
-
-/*
-void doAverage(){
-}
-
-void doSorting(){
-    
-}
-
-void printResults(){
-    // result: students who got the highest and lowest score.
-    cout << "The Highest Score: " << endl;
-    cout << "The Lowest Score: " << endl;
-    
-    // result: sort
-    // name the columns: #, NAME, AGE, SCORE1, SCORE2, SCORE3, AVG.
-    cout << "Sort by Avg. (from the highest to the lowest): " << endl;
-    cout << "#" << setw(12) << "NAME" << setw(8) << "AGE"<< setw(8) << "SCORE1" << setw(8) << "SCORE2" << setw(8) << "SCORE3" << setw(8) << "AVG.\n" << endl;
-}
-*/
 
 #endif /* process_h */
 
